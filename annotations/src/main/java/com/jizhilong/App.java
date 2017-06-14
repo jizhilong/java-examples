69f40d1f69541c452b7ad6813f9f28b01d4747c9
Ji.Zhilong <zhilongji@gmail.com>
2017-06-14 20:44:33 +0800

    add a really simple annotation example

---
+++ annotations/src/main/java/com/jizhilong/App.java
@@ -0,0 +1,16 @@
+package com.jizhilong;
+
+import com.jizhilong.MyAnnotations.Author;
+
+/**
+ * Hello world!
+ *
+ */
+@Author("jizhilong")
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ annotations/src/main/java/com/jizhilong/MyAnnotations.java
@@ -0,0 +1,16 @@
+/**
+ * Alipay.com Inc.
+ * Copyright (c) 2004-2017 All Rights Reserved.
+ */
+package com.jizhilong;
+
+/**
+ *
+ * @author zhilong.jzl
+ * @version $Id: MyAnnotations.java, v 0.1 2017年06月14日 8:38 PM zhilong.jzl Exp $
+ */
+public class MyAnnotations {
+    public @interface Author {
+        String value();
+    }
+}
