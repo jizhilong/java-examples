3314eeb712adb29b3cf6272bf6390f6f4bba44a6
Ji.Zhilong <zhilongji@gmail.com>
2017-06-14 21:04:42 +0800

    add a more advanced annotation example

--- annotations/src/main/java/com/jizhilong/App.java
+++ annotations/src/main/java/com/jizhilong/App.java
@@ -1,16 +1,109 @@
 package com.jizhilong;
 
 import com.jizhilong.MyAnnotations.Author;
+import com.jizhilong.MyAnnotations.Test;
+import com.jizhilong.MyAnnotations.TesterInfo;
+import com.jizhilong.MyAnnotations.TesterInfo.Priority;
+
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Method;
 
 /**
  * Hello world!
  *
  */
 @Author("jizhilong")
-public class App 
+public class App
 {
+    @TesterInfo(
+            priority = Priority.HIGH,
+            createdBy = "jizhilong",
+            tags = {"sales", "test"}
+    )
+    private static class TestExample {
+        @Test
+        void testA() {
+            if (true) {
+                throw new RuntimeException("This test always failed");
+            }
+        }
+
+        @Test(enabled = false)
+        void testB() {
+            if (false) {
+                throw new RuntimeException("This test always passed");
+            }
+        }
+
+        @Test(enabled = true)
+        void testC() {
+            if (10 > 1) {
+                // do nothins, this test always passed
+            }
+        }
+    }
+
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println("Testing...");
+
+        int passed = 0, failed = 0, count = 0, ignore = 0;
+
+        Class<TestExample> obj = TestExample.class;
+
+        // Process @TesterInfo
+        if (obj.isAnnotationPresent(TesterInfo.class)) {
+
+            Annotation annotation = obj.getAnnotation(TesterInfo.class);
+            TesterInfo testerInfo = (TesterInfo) annotation;
+
+            System.out.printf("%nPriority :%s", testerInfo.priority());
+            System.out.printf("%nCreatedBy :%s", testerInfo.createdBy());
+            System.out.printf("%nTags :");
+
+            int tagLength = testerInfo.tags().length;
+            for (String tag : testerInfo.tags()) {
+                if (tagLength > 1) {
+                    System.out.print(tag + ", ");
+                } else {
+                    System.out.print(tag);
+                }
+                tagLength--;
+            }
+
+            System.out.printf("%nLastModified :%s%n%n", testerInfo.lastModified());
+
+        }
+
+        // Process @Test
+        for (Method method : obj.getDeclaredMethods()) {
+
+            // if method is annotated with @Test
+            if (method.isAnnotationPresent(Test.class)) {
+
+                Annotation annotation = method.getAnnotation(Test.class);
+                Test test = (Test) annotation;
+
+                // if enabled = true (default)
+                if (test.enabled()) {
+
+                    try {
+                        method.invoke(obj.newInstance());
+                        System.out.printf("%s - Test '%s' - passed %n", ++count, method.getName());
+                        passed++;
+                    } catch (Throwable ex) {
+                        System.out.printf("%s - Test '%s' - failed: %s %n", ++count, method.getName(), ex.getCause());
+                        failed++;
+                    }
+
+                } else {
+                    System.out.printf("%s - Test '%s' - ignored%n", ++count, method.getName());
+                    ignore++;
+                }
+
+            }
+
+        }
+        System.out.printf("%nResult : Total : %d, Passed: %d, Failed %d, Ignore %d%n", count, passed, failed, ignore);
     }
 }
--- annotations/src/main/java/com/jizhilong/MyAnnotations.java
+++ annotations/src/main/java/com/jizhilong/MyAnnotations.java
@@ -4,6 +4,12 @@
  */
 package com.jizhilong;
 
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
 /**
  *
  * @author zhilong.jzl
@@ -12,5 +18,30 @@ package com.jizhilong;
 public class MyAnnotations {
     public @interface Author {
         String value();
+    }
+
+    @Retention(RetentionPolicy.RUNTIME)
+    @Target(ElementType.METHOD)
+    public @interface Test {
+        // should ignore this test?
+        public boolean enabled() default true;
+    }
+
+    @Retention(RetentionPolicy.RUNTIME)
+    @Target(ElementType.TYPE)
+    public @interface TesterInfo {
+        public enum Priority {
+            LOW,
+            MEDIUM,
+            HIGH
+        }
+
+        Priority priority() default Priority.MEDIUM;
+
+        String[] tags() default "";
+
+        String createdBy() default "jizhilong";
+
+        String lastModified() default "14/06/2017";
     }
 }
