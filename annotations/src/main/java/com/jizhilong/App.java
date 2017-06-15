f0c44d8ca07488a8b117accb34570671acacbab9
Ji.Zhilong <zhilongji@gmail.com>
2017-06-15 10:51:42 +0800

    improve annotation examples

--- annotations/src/main/java/com/jizhilong/App.java
+++ annotations/src/main/java/com/jizhilong/App.java
@@ -5,8 +5,11 @@ import com.jizhilong.MyAnnotations.Test;
 import com.jizhilong.MyAnnotations.TesterInfo;
 import com.jizhilong.MyAnnotations.TesterInfo.Priority;
 
-import java.lang.annotation.Annotation;
 import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * Hello world!
@@ -54,56 +57,36 @@ public class App
         // Process @TesterInfo
         if (obj.isAnnotationPresent(TesterInfo.class)) {
 
-            Annotation annotation = obj.getAnnotation(TesterInfo.class);
-            TesterInfo testerInfo = (TesterInfo) annotation;
+            TesterInfo testerInfo = (TesterInfo) obj.getAnnotation(TesterInfo.class);
 
             System.out.printf("%nPriority :%s", testerInfo.priority());
             System.out.printf("%nCreatedBy :%s", testerInfo.createdBy());
-            System.out.printf("%nTags :");
-
-            int tagLength = testerInfo.tags().length;
-            for (String tag : testerInfo.tags()) {
-                if (tagLength > 1) {
-                    System.out.print(tag + ", ");
-                } else {
-                    System.out.print(tag);
-                }
-                tagLength--;
-            }
-
+            System.out.printf("%nTags : %s", String.join(", ", Arrays.asList(testerInfo.tags())));
             System.out.printf("%nLastModified :%s%n%n", testerInfo.lastModified());
-
         }
 
-        // Process @Test
-        for (Method method : obj.getDeclaredMethods()) {
+        for (Method method: Arrays.stream(obj.getDeclaredMethods()).
+                filter(m -> m.isAnnotationPresent(Test.class)).
+                collect(Collectors.toList())) {
 
-            // if method is annotated with @Test
-            if (method.isAnnotationPresent(Test.class)) {
-
-                Annotation annotation = method.getAnnotation(Test.class);
-                Test test = (Test) annotation;
-
-                // if enabled = true (default)
-                if (test.enabled()) {
-
-                    try {
-                        method.invoke(obj.newInstance());
-                        System.out.printf("%s - Test '%s' - passed %n", ++count, method.getName());
-                        passed++;
-                    } catch (Throwable ex) {
-                        System.out.printf("%s - Test '%s' - failed: %s %n", ++count, method.getName(), ex.getCause());
-                        failed++;
-                    }
-
-                } else {
-                    System.out.printf("%s - Test '%s' - ignored%n", ++count, method.getName());
-                    ignore++;
+            Test test = method.getAnnotation(Test.class);
+            // if enabled = true (default)
+            if (test.enabled()) {
+                try {
+                    method.invoke(obj.newInstance());
+                    System.out.printf("%s - Test '%s' - passed %n", ++count, method.getName());
+                    passed++;
+                } catch (Throwable ex) {
+                    System.out.printf("%s - Test '%s' - failed: %s %n", ++count, method.getName(), ex.getCause());
+                    failed++;
                 }
 
+            } else {
+                System.out.printf("%s - Test '%s' - ignored%n", ++count, method.getName());
+                ignore++;
             }
-
         }
+
         System.out.printf("%nResult : Total : %d, Passed: %d, Failed %d, Ignore %d%n", count, passed, failed, ignore);
     }
 }
