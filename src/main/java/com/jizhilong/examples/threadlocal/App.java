35f974237b84c85813fa470643f1683d9d439e27
Ji.Zhilong <zhilongji@gmail.com>
2017-09-26 21:18:02 +0800

    add example for thread local

---
+++ src/main/java/com/jizhilong/examples/threadlocal/App.java
@@ -0,0 +1,26 @@
+package com.jizhilong.examples.threadlocal;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author jizhilong, {@literal <zlji@leyantech.com>}
+ * @date 26/09/2017
+ */
+public class App {
+  private static ThreadLocal<String> threadName = ThreadLocal.withInitial(() -> Thread.currentThread().getName());
+  private static ScheduledExecutorService executor = Executors.newScheduledThreadPool(4);
+
+  private static void printThreadName() {
+    System.out.println(threadName.get());
+  }
+
+  public static void main(String[] args) {
+    executor.schedule(App::printThreadName, 1, TimeUnit.SECONDS);
+    executor.schedule(App::printThreadName, 2, TimeUnit.SECONDS);
+    executor.schedule(App::printThreadName, 3, TimeUnit.SECONDS);
+    executor.schedule(App::printThreadName, 4, TimeUnit.SECONDS);
+  }
+}
